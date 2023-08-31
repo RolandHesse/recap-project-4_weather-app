6ee5fe567145ff02ca6757176886d87d7cee3b2e
Peet Thesing <p.thesing@posteo.de>
2023-08-31 10:53:01 +0200

    add css

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -1,11 +1,16 @@
-export default function List({ activities, isGoodWeather }) {
+export default function List({ activities, isGoodWeather, onDeleteActivity }) {
   if (isGoodWeather) {
     return (
       <>
         <h3>The weather is awesome! Go outside and:</h3>
         <ul>
           {activities.map((activity) => (
-            <li key={activity.id}>{activity.name}</li>
+            <li key={activity.id}>
+              {activity.name}
+              <button type="button" onClick={onDeleteActivity}>
+                X
+              </button>
+            </li>
           ))}
         </ul>
       </>
@@ -16,7 +21,12 @@ export default function List({ activities, isGoodWeather }) {
         <h3>Bad weather outside! Here's what you can do now:</h3>
         <ul>
           {activities.map((activity) => (
-            <li key={activity.id}>{activity.name}</li>
+            <li key={activity.id}>
+              {activity.name}{" "}
+              <button type="button" onClick={onDeleteActivity}>
+                X
+              </button>
+            </li>
           ))}
         </ul>
       </>
